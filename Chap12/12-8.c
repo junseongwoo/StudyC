b9fe317cc8a63d78becfdc4f4f4a0f1c1e9c62dc
junseongwoo <wnstjd4852@gmail.com>
2021-05-04 12:33:55 +0900

    0504 커밋

--- Chap12/12-8.c
+++ Chap12/12-8.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	int i = 0;
-	char str[20];
-	char ch;
+	char str1[80] = "strawberry";
+	char str2[80] = "apple";
+	char* ps1 = "banana";
+	char* ps2 = str2;
 
-	do
-	{
-		ch = getchar();
-		str[i] = ch;
-		i++;
-	} while (ch != '\n');
+	printf("최초 문자열 : %s\n", str1);
+	strcpy(str1, str2);
+	printf("바뀐 문자열: %s\n", str1);
 
-	str[i] = '\0';
+	strcpy(str1, ps1);
+	printf("바뀐 문자열 : %s\n", str1);
 
+	strcpy(str1, ps2);
+	printf("바뀐 문자열 : %s\n", str1);
+
+	strcpy(str1, "banana");
+	printf("바뀐 문자열 : %s\n", str1);
+	
 	return 0;
-
 }
